2b32f21b7a11543cfdd33c23789c005b5d2cb16d
HoTienQuoc <hotienquoc0429@gmail.com>
2025-11-26 22:42:39 +0700

    Complete Single Responsibility Principle

---
+++ SingleResponsibilityPrinciple/Persistence.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace SingleResponsibilityPrinciple;
+
+public class Persistence
+{
+    public void SaveToFile(Journal journal, string filename, bool overwrite = false)
+    {
+        if (overwrite || !File.Exists(filename))
+        {
+            File.WriteAllText(filename, journal.ToString());
+        }
+    }
+}
--- SingleResponsibilityPrinciple/Program.cs
+++ SingleResponsibilityPrinciple/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace SingleResponsibilityPrinciple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var journal = new Journal();
+            journal.AddEntry("I cried today.");
+            journal.AddEntry("I ate a bug.");
+
+            Console.WriteLine(journal);
+
+            var p = new Persistence();
+            var filename = "journal.txt";
+            p.SaveToFile(journal, filename, true);
+
+        }
+    }
+}
